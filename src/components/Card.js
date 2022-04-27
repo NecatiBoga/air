a6d157d0964f08fe2d4b4cc3b35f7accd525722c
enesbugrac <enesbc46@gmail.com>
2022-04-28 00:04:43 +0300

    ui update and firebase initiliaze

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,23 @@ import humidity from "../img/humidity.png";
 import co2 from "../img/co2.png";
 import pressure from "../img/pressure.png";
 import thermometer from "../img/thermometer.png";
+import { useEffect } from "react";
+import { db } from "../firebaseutils";
+import { useState } from "react";
 const Card = () => {
+  const [currentData, setCurrentData] = useState({});
+  useEffect(() => {
+    try {
+      db.collection("datasFromPI")
+        .orderBy("timestap", "desc")
+        .onSnapshot((snapshot) => {
+          console.log(snapshot.docs[0].data());
+          setCurrentData(snapshot.docs[0].data());
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
   return (
     <div>
       <div className="wrapper">
@@ -27,34 +43,30 @@ const Card = () => {
             </div>
 
             <div className="productImage">
-              <img src={sun} alt="product: ps5 controller image" />
+              <img src={sun} alt="" />
             </div>
           </div>
         </div>
 
         <div className="productSpecifications">
-          <h1> Weather Forecast </h1>
-          <p>
-            {" "}
-            The weather is warm enough to go out with our loved ones today....{" "}
-          </p>
+          <h1>Air Quality Values </h1>
 
           <div className="productFeatures">
             <div className="feature">
               <div className="featureIcon">
-                <img src={humidity} alt="product: ps5 controller image" />
+                <img src={humidity} alt="" />
               </div>
               <div className="featureText">
                 <p>
                   {" "}
                   <strong>HUMIDITY</strong>
                 </p>
-                <p>67%</p>
+                <p>{currentData.humidity}%</p>
               </div>
             </div>
             <div className="feature">
               <div className="featureIcon">
-                <img src={pressure} alt="product: ps5 controller image" />
+                <img src={pressure} alt="" />
               </div>
               <div className="featureText">
                 <p>
@@ -62,13 +74,13 @@ const Card = () => {
                   <strong>PRESSURE</strong>
                 </p>
                 <p>
-                  100 <sub>hPa</sub>
+                  {currentData.pressure} <sub>hPa</sub>
                 </p>
               </div>
             </div>
             <div className="feature">
               <div className="featureIcon">
-                <img src={co2} alt="product: ps5 controller image" />
+                <img src={co2} alt="" />
               </div>
               <div className="featureText">
                 <p>
@@ -76,26 +88,25 @@ const Card = () => {
                   <strong>CO2</strong>
                 </p>
                 <p>
-                  100 <sub>ppm</sub>
+                  {currentData.co2}
+                  <sub>ppm</sub>
                 </p>
               </div>
             </div>
 
             <div className="feature">
               <div className="featureIcon">
-                <img src={thermometer} alt="product: ps5 controller image" />
+                <img src={thermometer} alt="" />
               </div>
               <div className="featureText">
                 <p>
                   {" "}
                   <strong>Temperature</strong>
                 </p>
-                <p>25°C</p>
+                <p>{currentData.temperature}°C</p>
               </div>
             </div>
           </div>
-
-          
         </div>
       </div>
     </div>
